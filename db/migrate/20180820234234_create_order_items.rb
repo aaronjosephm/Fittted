c2d44c949484868992a52c5ca825e11a64d172b4
Aaron Mednick <aaronjosephm@gmail.com>
2018-08-20 20:25:45 -0400

    did some updates on the db

--- db/migrate/20180820234234_create_order_items.rb
+++ db/migrate/20180820234234_create_order_items.rb
@@ -2,6 +2,7 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
   def change
     create_table :order_items do |t|
       t.references :outfits, foreign_key: true
+      t.references :orders, foreign_key: true
 
       t.timestamps
     end
