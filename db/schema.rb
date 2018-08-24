f630c61b60a4516378ff979731afa9aa9f684616
Caroline Shearer <carolineshearer88@gmail.com>
2018-08-24 14:09:32 -0400

    login form done

--- db/schema.rb
+++ db/schema.rb
@@ -61,8 +61,8 @@ ActiveRecord::Schema.define(version: 2018_08_21_154621) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
     t.string "address"
+    t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
