f39e4a38539fdec4b6945a5c28f8aa12238430e6
Aaron Mednick <aaronjosephm@gmail.com>
2018-08-30 17:22:40 -0400

    strong admin params

--- app/admin/outfits.rb
+++ app/admin/outfits.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Outfit do
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
-# permit_params :list, :of, :attributes, :on, :model
+permit_params :name, :price, :description, :photo, :theme, :color, :body_type
 #
 # or
 #
